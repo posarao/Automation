8944b24cf70d51eb3a52a4b0dbeb87341a19e7e0
Posarao Kodi <32336564+posarao@users.noreply.github.com>
2024-08-26 20:25:10 +0530

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -10,22 +10,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 
 
+
 public class BrowserTest {
 
 	
 	@Test
 	public void getData()
 	{
-		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
-		driver.get("https://posadevopsweb-a9eyfkh2czhphhd5.eastus-01.azurewebsites.net/webapp/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
+		System.out.println("launching chrome browser"); 
+   		System.setProperty("webdriver.chrome.driver", driverPath);
+   		WebDriver driver = new ChromeDriver();
+   		driver.get("https://posadevopsweb-a9eyfkh2czhphhd5.eastus-01.azurewebsites.net/webapp/");
+   		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
 		Assert.assertTrue(text.equalsIgnoreCase("Posarao DevOps Learning 2024"));
 		driver.close();
-	
-		
 		
 	}
 }
